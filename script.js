a28bbf545ea9c22edb23c2116981d8bdedd8b421
VitFly <tommy.babcook@gmail.com>
2021-08-06 14:08:27 +0300

    4 com

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 'use strict'
 
-let userName = prompt('Кто там?', '');
+// let userName = prompt('Кто там?', '');
 
-    if (userName == 'Админ'){
+//     if (userName == 'Админ'){
 
-        let pass = prompt('Пароль?','');
+//         let pass = prompt('Пароль?','');
 
-        if(pass == 'Я главный'){
-            alert('Здравствуйте!');
-        }
-        else if(pass == null || ''){
-            alert('Отменено');
-        } 
-        else{
-            alert('Неверный пароль');
-        }
+//         if(pass == 'Я главный'){
+//             alert('Здравствуйте!');
+//         }
+//         else if(pass == null || ''){
+//             alert('Отменено');
+//         } 
+//         else{
+//             alert('Неверный пароль');
+//         }
 
         
-    }   else if(userName == null || userName == ''){
-        alert('Отменено');
-    }   else{
-        alert('Я вас не знаю')
-    }
+//     }   else if(userName == null || userName == ''){
+//         alert('Отменено');
+//     }   else{
+//         alert('Я вас не знаю')
+//     }
