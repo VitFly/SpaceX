c75a02c7ea9001a9ddaa1730d3e15f0e363e7ae5
VitFly <tommy.babcook@gmail.com>
2021-08-08 14:26:43 +0300

    com 6 js logic

--- script.js
+++ script.js
@@ -42,10 +42,41 @@
     // }
 
 
-    function checkAge(age) {
-        if (age > 18) {
-          return true;
-        } else {
-          return confirm('Родители разрешили?');
-        }
-      }
+    // function checkAge(age) {
+    //   if (age > 18) {
+    //     return true;
+    //   } else {
+    //     return confirm('Родители разрешили?');
+    //   }
+    // }
+
+
+  //   function getMin(a,b){
+  //     return Math.min(a,b);
+  //   }
+
+  //  let resultFunc = getMin(12,7);
+
+  //   console.log(resultFunc);
+
+
+  function getPow(){
+      let PowNum1 = prompt('Введите 1-е число','');
+      let PowNum2 = prompt('Введите 2-е число(возведение в степень)','');
+      let PowMath = Math.pow(PowNum1, PowNum2);
+      alert(PowMath);
+  }
+
+  getPow();
+
+//   getPow(3,6);
+
+//   let PowFunc = getPow;
+
+//   pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+
+    
+
+    
